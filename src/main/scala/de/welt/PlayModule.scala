7eed7520425126076efb277610cde345d08ed41c
thisismana <matthias.naber@weltn24.de>
2016-07-05 15:05:38 +0200

    eager binding

--- src/main/scala/de/welt/PlayModule.scala
+++ src/main/scala/de/welt/PlayModule.scala
@@ -1,7 +1,7 @@
 package de.welt
 
 import de.welt.services.configuration.{ContentClientConfig, ContentClientConfigImpl, SectionConfigurationService, SectionConfigurationServiceImpl}
-import de.welt.services.contentapi._
+import services.contentapi.{LegacySectionService, LegacySectionServiceImpl, _}
 import de.welt.services.s3.{S3, S3Impl}
 import play.api.inject.Module
 import play.api.{Configuration, Environment}
@@ -10,15 +10,16 @@ class PlayModule extends Module {
 
   override def bindings(environment: Environment, configuration: Configuration) = {
     Seq(
-        bind(classOf[ContentClientConfig]).to(classOf[ContentClientConfigImpl]),
-        bind(classOf[S3]).to(classOf[S3Impl]),
+      bind(classOf[ContentClientConfig]).to(classOf[ContentClientConfigImpl]).eagerly,
+      bind(classOf[ContentService]).to(classOf[ContentServiceImpl]).eagerly,
 
-        bind(classOf[ContentService]).to(classOf[ContentServiceImpl]),
-        bind(classOf[ContentEnrichmentService]).to(classOf[ContentEnrichmentServiceImpl]),
-        bind(classOf[SectionConfigurationService]).to(classOf[SectionConfigurationServiceImpl]),
-        bind(classOf[ContentSearchService]).to(classOf[ContentSearchServiceImpl]),
+      bind(classOf[S3]).to(classOf[S3Impl]),
 
-        bind[WeltContentClientLogic].to[WeltContentClient].eagerly
+      bind(classOf[ContentEnrichmentService]).to(classOf[ContentEnrichmentServiceImpl]),
+      bind(classOf[SectionConfigurationService]).to(classOf[SectionConfigurationServiceImpl]),
+      bind(classOf[LegacySectionService]).to(classOf[LegacySectionServiceImpl]),
+      bind(classOf[ContentSearchService]).to(classOf[ContentSearchServiceImpl])
+
     )
   }
 }
