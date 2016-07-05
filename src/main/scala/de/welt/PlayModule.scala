1d2851b0064d644847e221b9410a1f2a1f0c39a8
thisismana <matthias.naber@weltn24.de>
2016-07-05 15:01:11 +0200

    DI

--- src/main/scala/de/welt/PlayModule.scala
+++ src/main/scala/de/welt/PlayModule.scala
@@ -1,5 +1,8 @@
 package de.welt
 
+import de.welt.services.configuration.{ContentClientConfig, ContentClientConfigImpl, SectionConfigurationService, SectionConfigurationServiceImpl}
+import de.welt.services.contentapi._
+import de.welt.services.s3.{S3, S3Impl}
 import play.api.inject.Module
 import play.api.{Configuration, Environment}
 
@@ -7,7 +10,15 @@ class PlayModule extends Module {
 
   override def bindings(environment: Environment, configuration: Configuration) = {
     Seq(
-      bind[WeltContentClientLogic].to[WeltContentClient].eagerly
+        bind(classOf[ContentClientConfig]).to(classOf[ContentClientConfigImpl]),
+        bind(classOf[S3]).to(classOf[S3Impl]),
+
+        bind(classOf[ContentService]).to(classOf[ContentServiceImpl]),
+        bind(classOf[ContentEnrichmentService]).to(classOf[ContentEnrichmentServiceImpl]),
+        bind(classOf[SectionConfigurationService]).to(classOf[SectionConfigurationServiceImpl]),
+        bind(classOf[ContentSearchService]).to(classOf[ContentSearchServiceImpl]),
+
+        bind[WeltContentClientLogic].to[WeltContentClient].eagerly
     )
   }
 }
